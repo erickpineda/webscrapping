3194c05820a4b134f7fdd443de7a8824982858e7
Erick Pineda <epcloud2012@gmail.com>
2015-01-02 00:17:49 +0100

    Finiquito del programa.

--- webscrapping/src/main/java/net/erickpineda/webscrapping/App.java
+++ webscrapping/src/main/java/net/erickpineda/webscrapping/App.java
@@ -1,5 +1,8 @@
 package net.erickpineda.webscrapping;
 
+import java.io.BufferedReader;
+import java.io.FileNotFoundException;
+import java.io.FileReader;
 import java.io.FileWriter;
 import java.io.IOException;
 import java.io.PrintWriter;
@@ -12,81 +15,142 @@ import org.jsoup.nodes.Element;
 import org.jsoup.select.Elements;
 
 /**
- * @author Erick Pineda - Web Scrapping a la Wikipedia
- *
+ * @author Erick Pineda - Web Scrapping a la Wikipedia. Programa que extrae y
+ *         convierte la información de una tabla html a un fichero sql (URL a la
+ *         Wikipedia), crea una conexión con la BDD MySQL e inserta los datos en
+ *         una tabla creada.
  */
 public class App {
-
+	/**
+	 * Nombre que de la base de datos a conectar.
+	 */
 	private static String bd = "europa";
-	private static String user = "erick";
-	private static String password = "ies2015!";
+	/**
+	 * Nombre del usuario de la base de datos.
+	 */
+	private static String user = "myUser";
+	/**
+	 * Contraseña del usuario de la base de datos.
+	 */
+	private static String password = "myPass!";
+	/**
+	 * Nombre de host para el SGBDD.
+	 */
 	private static String host = "localhost";
+	/**
+	 * Clase que conecta conecta con la BDD y demás operaciones.
+	 */
 	private static ConectarBD conecta;
+	/**
+	 * Será la lista de los INSERTs que se guardarán en un fichero sql.
+	 */
 	private static List<String> lista;
-	
-	private static String nombreFichero = "inserts.sql";
-	private static FileWriter fichero = null;
-	private static PrintWriter escribirEnFichero = null;
-	
+	/**
+	 * Nombre que llevará el fichero a crear.
+	 */
+	private static String nombreFichero = "src/main/resources/inserts.sql";
+	/**
+	 * Permite crear un fichero de escritura.
+	 */
+	private static FileWriter ficheroAEscribir = null;
+	/**
+	 * Variable que escribirá en el fichero a crear o creado.
+	 */
+	private static PrintWriter out = null;
+	/**
+	 * Abre el fichero a leer.
+	 */
+	private static FileReader ficheroALeer = null;
+	/**
+	 * Variable que se encarga de ir línea a línea del fichero existente.
+	 */
+	private static BufferedReader in = null;
 
+	/**
+	 * Método principal del programa.
+	 */
 	public static void main(String[] args) {
 
 		try {
 			App.inicio();
 		} catch (IOException e) {
 			e.printStackTrace();
+		} catch (InterruptedException e) {
+			e.printStackTrace();
 		}
 
 	}
 
-	public static void inicio() throws IOException {
-		// Llamo al root del DOM
+	/**
+	 * Método que reune el correcto orden, del funcionamiento del programa.
+	 * 
+	 * @throws IOException
+	 *             Errores de excepciones.
+	 * @throws InterruptedException
+	 *             Lanzador de Excepción.
+	 */
+	public static void inicio() throws IOException, InterruptedException {
+
+		// Llamo al root del DOM para la URL
 		Document document = Jsoup.connect(
 				"http://ca.wikipedia.org/wiki/Llista_de_capitals_europees")
 				.get();
 
 		// Selecciono todas las etiquetas tr
-		Elements inputs = document.getElementsByTag("tr");
-		
-		
+		Elements inputs = document.select("tr");
+
+		// Extraer información de la Wikipedia
 		extraerDatosWikipedia(inputs);
+
+		// Crear el fichero SQL
 		crearFicheroSQL();
-		
-		for (String s : lista)
-			System.out.print(s);
 
-		
-
-		// Efectúo la conexción con la BDD
+		// Efectúo la conexión con la BDD
 		conecta = new ConectarBD(host, bd, user, password);
 		conecta.conectarBD();
 
 		// Siempre que la tabla no exista, creará una nueva
 		conecta.insertarDatos("CREATE TABLE IF NOT EXISTS paises ("
-				+ "pais varchar(30), codiiso varchar(4), superficie int(10),"
-				+ "habitants int(10), capital varchar(30))");
+				+ "pais varchar(30), codiiso varchar(4), superficie int(10), "
+				+ "habitants int(10), capital varchar(30));");
 
-		// Crear el fichero SQL
+		// Borra todos los datos sobre la tabla en la BDD
+		conecta.insertarDatos("TRUNCATE TABLE paises");
 
-		// Ejecutará fichero SQL
+		// Ejecutará el fichero SQL
+		insertarDatosSQL();
 
 		// Consulta sobre la base de datos
 		conecta.consultarDatos("SELECT * FROM paises");
+
+		// Cerrar la conexión sobre la BDD
 		conecta.desconectarDB();
 	}
 
+	/**
+	 * Método que se encarga de extraer la información de una tabla html y
+	 * guardarla en una lista.
+	 * 
+	 * @param entrada
+	 *            Parámetro que recibe los elementos seleccionados de la tabla.
+	 * @return lista Retorna una lista con la información de la tabla html.
+	 */
 	public static List<String> extraerDatosWikipedia(Elements entrada) {
 		lista = new ArrayList<String>();
 
 		for (Element cadaFila : entrada) {
 
+			// Recorrerá cada td en la tabla
 			if (cadaFila.select("td").size() > 0) {
 				Elements columnas = cadaFila.getElementsByTag("td");
-				int posColumna = 1;
-				String insert = "";
+				int posColumna = 1; // Cada columna en la tabla
+				String insert = ""; // Guarda la línea de texto, que será un
+									// INSERT
 
+				// Recorrerá cada columa por cada fila que exista en la tabla
 				for (Element columna : columnas) {
 
+					// Busco las etiquetas de nombre td
 					if (columna.tagName() == "td") {
 						if (posColumna == 2)
 							insert = "INSERT INTO paises VALUES('"
@@ -96,13 +160,14 @@ public class App {
 							insert = "'" + columna.text() + "', ";
 
 						if (posColumna == 4 || posColumna == 5)
-							insert = columna.text() + ", ";
+							insert = columna.text().replace(" ", "").toString()
+									+ ", ";
 
 						if (posColumna == 6)
 							insert = "'" + columna.text() + "');\n";
 
 					}
-					lista.add(insert);
+					lista.add(insert); // Agrego INSERT a la lista
 					posColumna++;
 				}
 			}
@@ -110,28 +175,63 @@ public class App {
 		return lista;
 	}
 
+	/**
+	 * Método que crea un fichero sql, con la información almacenada en la Lista @param
+	 * lista.
+	 */
 	public static void crearFicheroSQL() {
 		try {
-			
-			fichero = new FileWriter(nombreFichero);
-			escribirEnFichero = new PrintWriter(fichero);
-			
+
+			ficheroAEscribir = new FileWriter(nombreFichero);
+			out = new PrintWriter(ficheroAEscribir);
+
+			// Itera la lista y escribe sobre el fichero creado
 			for (int i = 0; i < lista.size(); i++)
-				escribirEnFichero.print(lista.get(i));
-			
+				out.print(lista.get(i));
+
 		} catch (IOException e) {
 			e.printStackTrace();
-		} finally{
-			if (fichero != null)
+		} finally {
+			if (ficheroAEscribir != null)
 				try {
-					fichero.close();
+					ficheroAEscribir.close();
 				} catch (IOException e) {
 					e.printStackTrace();
 				}
 		}
 	}
-	
-	public static void leerFicheroSQL(){
-		
+
+	/**
+	 * Método que se encarga de leer un fichero sql y efectuar las operaciones
+	 * sobre una BDD.
+	 */
+	public static void insertarDatosSQL() {
+
+		try {
+
+			String linea; // Línea en fichero
+			ficheroALeer = new FileReader(nombreFichero);
+			in = new BufferedReader(ficheroALeer);
+
+			if (in != null) {
+				linea = in.readLine();
+
+				while (linea != null) {
+					conecta.insertarDatos(linea); // INSERT INTO ...etc
+					linea = in.readLine();
+				}
+			}
+		} catch (FileNotFoundException e) {
+			e.printStackTrace();
+		} catch (IOException e) {
+			e.printStackTrace();
+		} finally {
+			if (ficheroALeer != null)
+				try {
+					ficheroALeer.close();
+				} catch (IOException e) {
+					e.printStackTrace();
+				}
+		}
 	}
 }
--- webscrapping/src/main/java/net/erickpineda/webscrapping/ConectarBD.java
+++ webscrapping/src/main/java/net/erickpineda/webscrapping/ConectarBD.java
@@ -5,16 +5,47 @@ import java.sql.ResultSet;
 import java.sql.SQLException;
 
 import com.mysql.jdbc.Connection;
+import com.mysql.jdbc.PreparedStatement;
 import com.mysql.jdbc.Statement;
 
 public class ConectarBD {
+	/**
+	 * Conector hacia el SGBDD.
+	 */
 	private static Connection conexion;
+	/**
+	 * Nombre que de la base de datos a conectar.
+	 */
 	private String bd;
+	/**
+	 * Nombre del usuario de la base de datos.
+	 */
 	private String user;
+	/**
+	 * Contraseña del usuario de la base de datos.
+	 */
 	private String password;
+	/**
+	 * Nombre de host para el SGBDD.
+	 */
 	private String host;
+	/**
+	 * Enlaza la conexión del host y bdd hacia el servidor.
+	 */
 	private String server;
 
+	/**
+	 * Constructor de la conexión hacia el SGBDD.
+	 * 
+	 * @param _host
+	 *            Será el nombre que llevará host a conectar.
+	 * @param bdd
+	 *            Nombre de la BDD a conectarse.
+	 * @param usuario
+	 *            Nombre del usuario que ingresa a la BDD.
+	 * @param pass
+	 *            Contraseña del usuario de la BDD.
+	 */
 	public ConectarBD(final String _host, final String bdd,
 			final String usuario, final String pass) {
 
@@ -23,9 +54,16 @@ public class ConectarBD {
 		this.user = usuario;
 		this.password = pass;
 		this.server = "jdbc:mysql://" + host + "/" + bd;
+
 	}
 
-	public void conectarBD() {
+	/**
+	 * Método que reúne la líneas de conexión hacia el SGBDD.
+	 * 
+	 * @throws InterruptedException
+	 *             Lanzador de Excepción.
+	 */
+	public void conectarBD() throws InterruptedException {
 
 		try {
 			Class.forName("com.mysql.jdbc.Driver");
@@ -33,23 +71,40 @@ public class ConectarBD {
 			conexion = (Connection) DriverManager.getConnection(server, user,
 					password);
 
-			System.out
-					.println("Conexión a base de datos " + server + " ... OK");
+			System.out.println("-> Conexión a base de datos " + server
+					+ " ... OK\n");
+
+			Thread.sleep(2000);
 
 		} catch (ClassNotFoundException e) {
+
 			e.printStackTrace();
-			System.out.println("Error cargando el Driver MySQL JDBC ... FAIL");
+			System.out
+					.println("-> Error cargando el Driver MySQL JDBC ... FAIL");
+
+			Thread.sleep(2000);
+
 		} catch (SQLException e) {
+
 			e.printStackTrace();
-			System.out.println("Imposible realizar conexión con " + server
+			System.out.println("-> Imposible realizar conexión con " + server
 					+ " ... FAIL");
+
+			Thread.sleep(2000);
 		}
 	}
 
+	/**
+	 * Método que realiza consultas SELECT sobre la BDD, recibiendo un String
+	 * como parámetro.
+	 * 
+	 * @param miConsulta
+	 *            Parámetro que será una SELECT, pasado como String.
+	 */
 	public void consultarDatos(final String miConsulta) {
 
 		Statement s;
-		ResultSet rs;
+		ResultSet rs; // Puntero o cursor a la fila actual
 
 		try {
 			s = (Statement) conexion.createStatement();
@@ -57,7 +112,7 @@ public class ConectarBD {
 
 			while (rs.next()) {
 				for (int i = 1; i <= rs.getMetaData().getColumnCount(); i++)
-					System.out.print(rs.getString(i) + " | ");
+					System.out.print(" " + rs.getString(i) + " |");
 				System.out.println();
 			}
 
@@ -66,27 +121,41 @@ public class ConectarBD {
 		}
 	}
 
+	/**
+	 * Método que se encarga de insertar datos a la base de datos.
+	 * 
+	 * @param miConsulta
+	 *            Parámetro que será un INSERT, pasado como String.
+	 */
 	public void insertarDatos(final String miConsulta) {
 
-		Statement s;
+		PreparedStatement s;
 
 		try {
-			s = (Statement) conexion.createStatement();
-			s.executeUpdate(miConsulta);
+			s = (PreparedStatement) conexion.prepareStatement(miConsulta);
+			s.execute(miConsulta);
 
 		} catch (SQLException e) {
+			try {
+				conexion.rollback(); // Rollback si el INSERT es erróneo
+			} catch (SQLException e1) {
+				e1.printStackTrace();
+			}
 			e.printStackTrace();
 		}
 	}
 
+	/**
+	 * Método que cierra la conexión de la BDD.
+	 */
 	public void desconectarDB() {
 
 		try {
 			conexion.close();
 		} catch (SQLException e) {
-			System.out.println("Cerrar conexión con " + server + " ... OK");
+			System.out.println("-> Cerrar conexión con " + server + " ... OK");
 			e.printStackTrace();
-			System.out.println("Imposible cerrar conexión ... FAIL");
+			System.out.println("-> Imposible cerrar conexión ... FAIL");
 		}
 	}
 }
